fe0aad6d2ef123a32f72dd3cf0754fca13f8d461
rushanthbala <58292966+rushanthbala@users.noreply.github.com>
2020-06-28 19:55:32 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import  Username from './pages/username'
-import  App from './pages/App'
-
-
-ReactDOM.render(<App/>,document.getElementById('root'))
-
-
